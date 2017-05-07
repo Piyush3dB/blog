89d3bd57562df41582d522d996d7864427b5d17e
Piyush Singh <mrsinghpiyush@gmail.com>
2017-05-07 20:12:52 +0100

    wip.  Use better theme

--- utils/typography.js
+++ utils/typography.js
@@ -1,7 +1,29 @@
 import Typography from 'typography'
-import Wordpress2016 from 'typography-theme-wordpress-2016'
+let theme = require("typography-theme-de-young").default
+const CodePlugin = require("typography-plugin-code").default
 
-const typography = new Typography(Wordpress2016)
+theme.plugins = [new CodePlugin()]
+theme.baseLineHeight = 1.4
+theme.blockMarginBottom = 0.75
+theme.overrideThemeStyles = ({ rhythm }) => ({
+  "blockquote > h1, blockquote > h2, blockquote > h3, blockquote > h4": {
+    marginTop: 0,
+  },
+  "li > p": {
+    marginBottom: rhythm(1 / 2),
+  },
+  "p code": {
+    fontSize: "75%",
+  },
+  "tt,code": {
+    fontSize: "85%",
+  },
+  pre: {
+    lineHeight: 1.22,
+  },
+})
+
+const typography = new Typography(theme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
